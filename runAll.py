979eea019d14a858ab804042cf253c730afe414e
Michael Hahn <mhahn@Michaels-MBP-2.fritz.box>
2025-08-13 13:52:38 +0200

    all model fits

--- runAll.py
+++ runAll.py
@@ -3,8 +3,10 @@ import random
 import subprocess
 import os
 script = sys.argv[1]
-
-
+if len(sys.argv) > 2:
+   REG = sys.argv[2]
+else:
+   REG = "10.0"
 for _ in range(10000):
    P = random.choice([0,1,2,4,6,8])
    fold = random.randint(0,9)
@@ -13,9 +15,9 @@ for _ in range(10000):
      targetScript = script.replace(".py", "_L0.py")
    elif P == 1:
      targetScript = script.replace(".py", "_L1.py")
-   targetFile = f"losses/{targetScript}_{P}_{fold}_10.0_180.txt"
+   targetFile = f"losses/{targetScript}_{P}_{fold}_{REG}_180.txt"
    if os.path.exists(targetFile):
        continue
    print(f"Running {targetScript} with P={P}, fold={fold}")
-   subprocess.call(["/Users/mhahn/anaconda3/envs/python39/bin/python3", targetScript, str(P), str(fold), "10.0", "180"])
+   subprocess.call(["/Users/mhahn/anaconda3/envs/python39/bin/python3", targetScript, str(P), str(fold), REG, "180"])
 
